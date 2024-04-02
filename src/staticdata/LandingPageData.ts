87e7c12ceae0d4c5884f671fbac4ceb807200430
Gaurav Kumar <gauravmac@Gauravs-MacBook-Air.local>
2024-04-03 02:01:14 +0530

    added landing page very basic without signin

---
+++ src/staticdata/LandingPageData.ts
@@ -0,0 +1,40 @@
+import { FaIcons } from "react-icons/fa"
+
+export const layoutPageData = [
+    {
+        id: 1,
+        heading: "Talk With Portfolio",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales vehicula justo id ornare. In hac habitasse platea dictumst. Fusce sagittis euismod lorem, a fermentum justo lacinia nec.",
+        icon: "",
+    },
+    {
+        id: 2,
+        heading: "Manage Portfolio",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales vehicula justo id ornare. In hac habitasse platea dictumst. Fusce sagittis euismod lorem, a fermentum justo lacinia nec.",
+        icon: "",
+    },
+    {
+        id: 3,
+        heading: "Buy Assets",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales vehicula justo id ornare. In hac habitasse platea dictumst. Fusce sagittis euismod lorem, a fermentum justo lacinia nec.",
+        icon: "",
+    },
+    {
+        id: 4,
+        heading: "Learn and Explore",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales vehicula justo id ornare. In hac habitasse platea dictumst. Fusce sagittis euismod lorem, a fermentum justo lacinia nec.",
+        icon: "",
+    },
+    {
+        id: 5,
+        heading: "News",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales vehicula justo id ornare. In hac habitasse platea dictumst. Fusce sagittis euismod lorem, a fermentum justo lacinia nec.",
+        icon: "",
+    },
+    {
+        id: 6,
+        heading: "All in One Space for Assets",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales vehicula justo id ornare. In hac habitasse platea dictumst. Fusce sagittis euismod lorem, a fermentum justo lacinia nec.",
+        icon: "",
+    }
+]
