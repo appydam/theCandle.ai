d8bbd7fce7a9b7467e361d45023283adca79b06a
Arpit Dhamija <arpitdhamija@Arpits-MacBook-Air.local>
2024-04-05 11:31:42 +0530

    bg, foot, navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,24 +7,27 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div className="mx-auto translate-y-6">
-      <div className="bg-[conic-gradient(var(--tw-gradient-stops))] from-gray-600 via-gray-400 to-gray-200 p-4 rounded-lg fixed w-[60%] top-0 left-1/2 transform -translate-x-1/2">
+      <div className="bg-[conic-gradient(var(--tw-gradient-stops))] from-blue-200 via-blue-300 to-blue-400 border border-white p-4 rounded-lg fixed w-[80%] top-0 left-1/2 transform -translate-x-1/2">
         <nav className="flex items-center justify-between">
           <div className="text-gray-600 text-xl font-bold flex">
-          <FileText className='text-gray-600 w-8 h-8 mr-2' /> 
-          <UserButton afterSignOutUrl='/'/>
+            <FileText className='text-gray-600 w-8 h-8 mr-2' />
+            <UserButton afterSignOutUrl='/' />
           </div>
-          <div className="flex space-x-4">
-            <a href="/" className="text-white">
-              Home
+          <div className="flex space-x-24">
+            <a href="/" className="text-gray-600 font-semibold">
+              Talk With Portfolio
             </a>
-            <a href="/about" className="text-white">
-              About
+            <a href="/about" className="text-gray-600 font-semibold">
+              Buy
             </a>
-            <a href="/tech" className="text-white">
-              Tech
+            <a href="/tech" className="text-gray-600 font-semibold">
+              Manage Portfolio
             </a>
-            <a href="/contact" className="text-white">
-              Contact
+            <a href="/contact" className="text-gray-600 font-semibold">
+              News
+            </a>
+            <a href="/contact" className="text-gray-600 font-semibold">
+              Learn
             </a>
           </div>
         </nav>
